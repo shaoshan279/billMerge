cb1f438186710f3900ad3f1684fb30394715f28d
李文广 <8553@dahantc.com>
2024-03-20 16:40:09 +0800

    转服务

---
+++ src/main/java/org/bill/BillMergeApplication.java
@@ -0,0 +1,17 @@
+package org.bill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类描述：
+ * @author 8553
+ * @date 2024-03-20 15:42:24
+ * 版权所有 Copyright www.dahantc.com
+ */
+@SpringBootApplication
+public class BillMergeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BillMergeApplication.class, args);
+	}
+}
